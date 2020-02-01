dbcad23b53f241cfc004d24bb70a7cc3b389b15d
Amanda Misjuwar <amandabmisjuwar@gmail.com>
2020-02-01 10:33:43 -0500

    Added onClick for DefineUser to Login

--- src/components/DefineUser.js
+++ src/components/DefineUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/DefineUser.css'
 import { Button, Grid } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
 
 function DefineUser() {
   const fundbearLogo = 'images/fundbear_logo.png'
@@ -10,9 +11,13 @@ function DefineUser() {
         <Grid.Column>
           <img src={fundbearLogo} alt="fundbear logo"></img>
           <p>Which fundbear are you?</p>
-          <Button content='DONOR' fluid size='big' secondary />
+          <Link to='/login'>
+            <Button content='DONOR' fluid size='big' onClick='' secondary />
+          </Link>
           <br />
-          <Button content='ORGANIZATION' fluid size='big' secondary />
+          <Link to='login'>
+            <Button content='ORGANIZATION' fluid size='big' secondary />
+          </Link>
         </Grid.Column>
       </Grid>
     </div>
