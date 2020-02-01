62021541cc9d9d24905b49378fe9170dec756cf3
Michelle Pham <m_vpham@M-vpham.local>
2020-02-01 02:06:41 -0500

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { Button } from 'semantic-ui-react';
 
 function App() {
